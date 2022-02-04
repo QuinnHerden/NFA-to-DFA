26f96491b29a645347d274ee2fd83bdaa4208019
Quinn Herden <herdenq@gmail.com>
2022-02-03 19:56:43 -0500

    add file read-in

--- toDfa.py
+++ toDfa.py
@@ -1,28 +1,75 @@
+import sys
+
 # collect nfa inputs
-alphabet = input('enter alphabet, space separated\n').split()
-alphabet.sort()
+if len(sys.argv) > 1:
+    # file specified
+    infile = open(sys.argv[1], 'r')
+    source = infile.read().splitlines()
 
-nfaStates = input('enter states, space separated\n').split()
-nfaStates.sort()
+    nfaStarts = list(set(source[0].split(', ')))
+    nfaStarts[0] = nfaStarts[0][1:]
+    nfaStarts[-1] = nfaStarts[-1][:-1]
+    nfaStarts.sort()
+    
+    nfaAccepts = list(set(source[1].split(', ')))
+    nfaAccepts[0] = nfaAccepts[0][1:]
+    nfaAccepts[-1] = nfaAccepts[-1][:-1]
+    nfaAccepts.sort()
 
-nfaStarts = input('enter start states, space separated\n').split()
-nfaStarts.sort()
+    transitionTable = {}
 
-nfaAccepts = input('enter accept states, space separated\n').split()
-nfaAccepts.sort()
+    nfaStates = set()
+    alphabet = set()
 
-transitionTable = {}
+    for line in source[2:]:
+        if line:
+            triple = line.split()
+
+            nfaStates.add(triple[0])
+            alphabet.add(triple[1])
+            nfaStates.add(triple[2])
+
+            if triple[0] not in transitionTable.keys():
+                transitionTable[triple[0]] = {}
+
+            transitionTable[triple[0]][triple[1]] = triple[2:]
+            transitionTable[triple[0]][triple[1]].sort()
+    
+    alphabet = list(alphabet)
+    alphabet.sort()
+
+    nfaStates = list(nfaStates)
+    nfaStates.sort()            
+
+    for state in nfaAccepts or nfaStarts:
+        if state not in transitionTable.keys():
+                transitionTable[state] = {}
+                
+                for symbol in alphabet:
+                    transitionTable[state][symbol] = []
+
+else:
+    # cmd line specified
+    alphabet = input('enter alphabet, space separated\n').split()
+    alphabet.sort()
+
+    nfaStates = input('enter states, space separated\n').split()
+    nfaStates.sort()
+
+    nfaStarts = input('enter start states, space separated\n').split()
+    nfaStarts.sort()
+
+    nfaAccepts = input('enter accept states, space separated\n').split()
+    nfaAccepts.sort()
+
+    transitionTable = {}
+    print('provide a space separated list of next state, given current state and input symbol')
+    for state in nfaStates:
+        transitionTable[state] = {}
+        for symbol in alphabet:
+            transitionTable[state][symbol] = input('%s %s -> ' % (state, symbol)).split()
 
 print()
-
-print('provide a space separated list of next state, given current state and input symbol')
-for state in nfaStates:
-    transitionTable[state] = {}
-    for symbol in alphabet:
-        transitionTable[state][symbol] = input('%s %s -> ' % (state, symbol)).split()
-
-print()
-
 
 # initialize translation supports
 stateCounter = 1
@@ -72,7 +119,13 @@ while (nfaQueue):
 
 
 # write start state, accept states, and transitions
-f = open("dfa.fa", "w")
+if len(sys.argv) > 1:
+    outputFilename = sys.argv[1].split('.')[0] + '.fa'
+else:
+    outputFilename = 'out.fa'
+
+f = open(outputFilename, "w")
+
 f.write(str(nfa_dfa[str(nfaStarts)]))
 
 acceptsString = '\n{'
